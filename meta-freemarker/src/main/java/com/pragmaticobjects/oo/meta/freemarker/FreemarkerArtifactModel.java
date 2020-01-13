47206c07febf8fb67f6675d5c34219fe4fd12cde
Kapralov Sergey <skapralov@mail.ru>
2020-01-14 02:45:15 +0300

    File headers updated

--- meta-freemarker/src/main/java/com/pragmaticobjects/oo/meta/freemarker/FreemarkerArtifactModel.java
+++ meta-freemarker/src/main/java/com/pragmaticobjects/oo/meta/freemarker/FreemarkerArtifactModel.java
@@ -1,8 +1,8 @@
 /*-
  * ===========================================================================
- * project-name
+ * meta-freemarker
  * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
- * Copyright (C) 2019 Kapralov Sergey
+ * Copyright (C) 2019 - 2020 Kapralov Sergey
  * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
