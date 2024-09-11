21c23179f5b38fe14c16a8c93f569cce532e51e8
Kapralov Sergey <skapralov@mail.ru>
2024-09-11 15:11:25 +0300

    147: issue fix

---
+++ oo-equivalence/equivalence-codegen/src/main/java/com/pragmaticobjects/oo/equivalence/codegen/matchers/MatchInheritedClass.java
@@ -0,0 +1,45 @@
+/*-
+ * ===========================================================================
+ * equivalence-codegen
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ * Copyright (C) 2019 - 2024 Kapralov Sergey
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ * ============================================================================
+ */
+package com.pragmaticobjects.oo.equivalence.codegen.matchers;
+
+import net.bytebuddy.matcher.ElementMatchers;
+
+/**
+ * Match class that is inheritor. In other words, match a class, parent of which is something different from
+ * the {@link Object}. Typically, for these classes, EObjectHint is disallowed, with some exceptions out of the rule.
+ *
+ * The class exists to cover these exceptions. For example, records are formally allowed to be hinted. Yet, they are not directly
+ * subclassed from {@link Object}.
+ */
+public class MatchInheritedClass extends MatchSuperClass {
+    public MatchInheritedClass() {
+        super(
+            ElementMatchers.not(
+                ElementMatchers.is(Object.class)
+            )
+        );
+    }
+}
